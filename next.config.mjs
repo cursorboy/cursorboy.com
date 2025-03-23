fd0e71e0b07b4d762a0b65e5fb4639160beac487
Piam Parekh <piamparekh17@gmail.com>
2025-03-23 14:13:46 -0700

    again ughhhh

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,6 @@
-let userConfig = undefined
-try {
-  userConfig = await import('./v0-user-next.config')
-} catch (e) {
-  // ignore error
-}
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -21,6 +15,13 @@ const nextConfig = {
     parallelServerBuildTraces: true,
     parallelServerCompiles: true,
   },
+}
+
+let userConfig = undefined
+try {
+  userConfig = await import('./v0-user-next.config.js')
+} catch (e) {
+  // ignore error
 }
 
 mergeConfig(nextConfig, userConfig)
